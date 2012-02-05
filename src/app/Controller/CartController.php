8343bfb7692b2a7446d84a8f1c539e7c2665fe32
Ales Matijevic <ales.matijevic@gmail.com>
2012-02-05 13:26:57 +0100

    Added cart functionality with checkout (creates new order)

--- src/app/Controller/CartController.php
+++ src/app/Controller/CartController.php
@@ -1,11 +1,22 @@
 <?php
 class CartController extends AppController 
 {
-
-	public function index() 
+	public function index()
 	{
 		$kosarica  = $this->Session->read("kosarica");
 		$this->set('kosarica',$kosarica);
+		
+		$items = array();
+   		$this->loadModel('Item');
+		foreach($kosarica as $item_id => $item_count)
+ 		{
+			
+			$kitem['Item'] = $this->Item->findById($item_id);
+			$kitem['Count'] = $item_count;
+			array_push($items, $kitem); 
+		}
+		$this->set('items',$items);
+		
 	}
 
 	public function add($id = null, $count = null)
@@ -20,25 +31,52 @@ class CartController extends AppController
 			$kosarica[$id] = $kosarica[$id]+$count;
 		$this->Session->write("kosarica",$kosarica);
 		$this->redirect(array('controller' => 'items', 'action' => 'index'));
-		echo "ITEMS:";
-		print_r($kosarica);
 	}
 
-	public function remove($id=null)
+
+	public function edit()
+	{
+		$edit_items = $this->data['edit_items'];
+		$remove_items = $this->data['remove_items'];
+
+		$kosarica  = $this->Session->read("kosarica");
+		if(!isset($kosarica)) $kosarica = array();
+		foreach($edit_items as $item_id => $item_count)
+		{
+			$kosarica[$item_id] = $item_count;
+		}
+		foreach($remove_items as $item_id => $remove)
+		{
+			if($remove) unset($kosarica[$item_id]);
+		}
+
+		$this->Session->write("kosarica",$kosarica);
+		$this->redirect(array('controller' => 'cart', 'action' => 'index'));
+	}
+
+	public function checkout($id=null)
 	{
 		$kosarica  = $this->Session->read("kosarica");
-		if(!isset($kosarica))
-			$kosarica = array();
-
-		if(isset($kosarica[$id])) 
-		{		
-			$kosarica[$id] = $kosarica[$id]-1;
-			if($kosarica[$id] == 0) 
-				unset($kosarica[$id]);
+		$this->set('kosarica',$kosarica);
+		$items = array();
+		$this->loadModel('Order');
+		$this->loadModel('ItemsOrder');
+		$this->Order->create();
+		$this->Order->set('user_id', AuthComponent::user('id'));
+		$this->Order->set('date', date( 'Y-m-d H:i:s'));
+		$this->Order->set('state','ODDANO');
+		$this->Order->save();
+		$new_order_id = $this->Order->id;
+		foreach($kosarica as $item_id => $item_count)
+ 		{
+			$this->ItemsOrder->create();
+			$this->ItemsOrder->set('order_id', $new_order_id);
+			$this->ItemsOrder->set('item_id', $item_id);
+			$this->ItemsOrder->set('quantity',$item_count);
+			$this->ItemsOrder->save();
 		}
-		$this->Session->write("kosarica",$kosarica);
-		echo "ITEMS:";
-		print_r($kosarica);
+		$this->Session->write("kosarica",array());
+		$this->redirect(array('controller' => 'orders', 'action' => 'index'));
 	}
 
 	public function clear($id=null)
--- src/app/View/Cart/index.ctp
+++ src/app/View/Cart/index.ctp
@@ -1,8 +1,36 @@
 <div>
 	<div>
-		Cart
-
-		<?php print_r($kosarica) ?>
-
+		<?php echo $this->Form->create(null, array('url' => array('controller' => 'cart', 'action' => 'edit'))); 
+		foreach ($items as $kitem): ?>		
+		<div>			
+			<?php	
+				$item = $kitem['Item'];
+				$item_id = $item['Item']['id'];
+				$item_image_url = "/img/no-image.gif";
+				if(isset($item['Image'][0]['image_url'])){
+				$item_image_url = $item['Image'][0]['image_url'];
+				}
+			?>
+			<img width="20px" height="20px" src="<?php echo $item_image_url; ?>"/>
+            <?php
+				echo $item['Item']['name']; 
+				echo " | ";				
+				echo $item['Item']['price'];
+				echo " | ";				
+				echo $this->Form->input('remove', array('type' => 'checkbox', 'id' => 'remove_items['.$item_id.']', 'name' => 'remove_items['.$item_id.']'));
+				echo $this->Form->input('|', array('id' => 'edit_items['.$item_id.']', 'name' => 'edit_items['.$item_id.']', 'value' => $kitem['Count']));
+			?>
+		</div>			
+		<?php endforeach; 
+			echo $this->Form->end(__('Accept cart changes'));	
+		?>		
+	</div>
+	<div>
+		<?php 
+			echo $this->Form->postLink("Checkout", array('controller' => 'cart', 'action' => 'checkout'),
+			array(),"Are you sure you wish to checkout?");
+		?>
 	</div>
 </div>
+
+
